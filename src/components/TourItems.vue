a1499e42aa6145a4f055ef6f4eac0a6d3b995a74
gkhabada <gadzhidadaev95@gmail.com>
2020-03-01 09:14:16 +0300

    start vue cli

---
+++ src/components/TourItems.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <section class="tour-items">
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'TourItems'
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
